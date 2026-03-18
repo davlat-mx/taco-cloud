e96f16dfb30d58d38bc43b872cfd257663bebe82
Davlat <davlatbek.muxtorov@octo.uz>
2026-03-18 20:26:55 +0500

    initial commit chapter-1

---
+++ src/test/java/org/dave/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dave.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
