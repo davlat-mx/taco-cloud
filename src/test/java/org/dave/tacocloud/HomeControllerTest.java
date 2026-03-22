9e8b2f1cfb6c17a6263eb34924a97e1441d3288c
Davlat <davlatbek.muxtorov@octo.uz>
2026-03-22 23:58:29 +0500

    added /design /order chapter-2

--- src/test/java/org/dave/tacocloud/HomeControllerTest.java
+++ src/test/java/org/dave/tacocloud/HomeControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 public class HomeControllerTest {
 
     @Autowired
